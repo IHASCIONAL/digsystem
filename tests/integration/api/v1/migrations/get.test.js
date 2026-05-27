674a006b3b7937076b6777fad3a6b02aeea47448
IHAS <ismaelhenriqueeu@gmail.com>
2026-05-26 22:54:53 -0300

    refactor: use `orchestrator.cleanDatabase()` in tests

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -9,6 +9,7 @@ beforeAll(async () => {
 describe("GET /api/v1/migrations", () => {
 describe("Anonymous user", () => {
 test("Retrieving pending migrations", async () => {
+test("GET to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
   expect(response.status).toBe(200);
 
@@ -19,6 +20,4 @@ test("Retrieving pending migrations", async () => {
   expect(Array.isArray(responseBody)).toBe(true);
 
   expect(responseBody.length).toBeGreaterThan(0);
-});
-});
 });
