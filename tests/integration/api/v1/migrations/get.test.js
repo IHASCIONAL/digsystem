IHASCIONAL/tabnews_js

test: rename all descriptions to use new pattern

tests/integration/api/v1/migrations/get.test.js
@@ -6,7 +6,9 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public;");
 });
 
-test("GET to /api/v1/migrations should return 200", async () => {
+describe("GET /api/v1/migrations", () => {
+describe("Anonymous user", () => {
+test("Running pending migrations", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
   expect(response.status).toBe(200);
 
@@ -17,4 +19,6 @@ test("GET to /api/v1/migrations should return 200", async () => {
   expect(Array.isArray(responseBody)).toBe(true);
 
   expect(responseBody.length).toBeGreaterThan(0);
+});
+});
 });

IHASCIONAL/digsystem

tests/integration/api/v1/migrations/get.test.js
@@ -6,7 +6,9 @@ beforeAll(async () => {
   await orchestrator.clearDatabase();
 });
 
-test("GET to /api/v1/migrations should return 200", async () => {
+describe("GET /api/v1/migrations", () => {
+describe("Anonymous user", () => {
+test("Retrieving pending migrations", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
   expect(response.status).toBe(200);
 
@@ -17,4 +19,6 @@ test("GET to /api/v1/migrations should return 200", async () => {
   expect(Array.isArray(responseBody)).toBe(true);
 
   expect(responseBody.length).toBeGreaterThan(0);
+});
+});
 });