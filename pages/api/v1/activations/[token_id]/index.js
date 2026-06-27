1f2ebac5f7418a4305c2b04009fe37f6e931737c
IHAS <ismaelhenriqueeu@gmail.com>
2026-06-27 20:03:53 -0300

    feat: require `create:user` to access `/api/v1/users`

--- pages/api/v1/activations/[token_id]/index.js
+++ pages/api/v1/activations/[token_id]/index.js
@@ -3,8 +3,8 @@ import controller from "infra/controller.js";
 import activation from "models/activation";
 
 const router = createRouter();
-
-router.patch(patchHandler);
+router.use(controller.injectAnonymousOrUser);
+router.patch(controller.canRequest("read:activation_token"), patchHandler);
 
 export default router.handler(controller.errorHandlers);
 
@@ -13,10 +13,11 @@ async function patchHandler(request, response) {
 
   const validActivationToken =
     await activation.findOneValidById(activationTokenId);
-  const usedActivationToken =
-    await activation.markTokenAsUsed(activationTokenId);
 
   await activation.activateUserByUserId(validActivationToken.user_id);
+
+  const usedActivationToken =
+    await activation.markTokenAsUsed(activationTokenId);
 
   return response.status(200).json(usedActivationToken);
 }
